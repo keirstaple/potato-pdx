6daa756e3c4070f57504fc6b6e64bd3530a97d46
Keir Staple <keirstaple@comcast.net>
2017-04-13 14:31:35 -0700

    center video and add button to close the window

--- src/views/components/VideoPlayer.js
+++ src/views/components/VideoPlayer.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactPlayer from 'react-player';
 import { connect } from 'react-redux';
+import FontAwesome from 'react-fontawesome';
 
 import { VideoColumns } from '../index';
 
 import { videoId } from './../../state';
 
 class VideoPlayer extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   renderVideo() {
     return(
-      <ReactPlayer url={`https://vimeo.com/${this.props.videoId}`} height='500px' width='800px' style={{}} />
+      <ReactPlayer url={`https://vimeo.com/${this.props.videoId}`} height='500px' width='800px' style={{position: 'absolute', left: '50%', top: '50%', marginLeft: '-30vw', marginTop: '-35vh'}} />
     )
+  }
+
+  handleClick() {
+    return this.context.router.push('/');
   }
 
   render() {
@@ -18,8 +27,16 @@ class VideoPlayer extends Component {
       <div>
         <VideoColumns />
         <div style={{position: 'absolute', height: '100vh', width: '97.6vw', marginTop: '-100vh', backgroundColor: 'rgba(0, 0, 0, 0.85)', zIndex: '1'}}>
+          <FontAwesome
+            className="times-icon"
+            name="window-close-o"
+            size="2x"
+            style={{color: 'white', position: 'absolute', left: '50%', top: '50%', marginLeft: '40vw', marginTop: '-40vh', zIndex: '2'}}
+            onClick={this.handleClick.bind(this)}
+          />
           { this.renderVideo() }
         </div>
+
       </div>
     )
   }
