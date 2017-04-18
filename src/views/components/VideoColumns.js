5b9a0c305f4c0b3cc60219bda9361c3798d0da7c
Keir Staple <keirstaple@comcast.net>
2017-04-17 17:40:09 -0700

    add a watch me link on hover

--- src/views/components/VideoColumns.js
+++ src/views/components/VideoColumns.js
@@ -4,9 +4,33 @@ import { Link } from 'react-router';
 import { getVideosThunk, videoData } from './../../state';
 
 class VideoColumns extends Component {
+  constructor() {
+    super()
+    this.state = { }
+  }
   componentWillMount() {
     //grabs posts when component loads
     this.props.getVideos();
+  }
+  hoverEvent(value, idx) {
+    // console.log('idx', idx);
+    // const linkDisplay = `linkDisplay${idx}`;
+    // console.log('linkDisplay', linkDisplay);
+    // const stateDisplay = 'this.state.linkDisplay'+idx;
+    // console.log('stateDisplay', stateDisplay)
+    // if(stateDisplay === 'none') {
+    //   this.setState({ [linkDisplay]: 'block' })
+    //   console.log('state', this.state)
+    // } else {
+    //   this.setState({ [linkDisplay]: 'none' })
+    //   console.log('state', this.state)
+    // }
+    if(value === undefined) {
+      value = 'none'
+    }
+    this.setState({ [idx]: value })
+    console.log(value);
+    return value
   }
   renderList() {
     let featuredVideos = [];
@@ -22,9 +46,20 @@ class VideoColumns extends Component {
     return featuredVideos.map((item, idx) => {
       const thumbnail = item.pictures.sizes[5].link;
       const columnWidth = 100 / featuredVideos.length
+      // let displayVersion = this.hoverEvent();
+      // console.log('displayVersion', displayVersion)
+      const displayVersion = this.state[idx] || 'none';
+      console.log('displayVersion', displayVersion)
       return(
-          <Link className="video-column" to={`${item.uri}`} key={idx} style={{backgroundImage: `url(${thumbnail})`, backgroundPosition: 'center', height: '100vh', width: `${columnWidth}vw`}}>
-          </Link>
+          <div
+            className="video-column"
+            to={`${item.uri}`}
+            key={idx}
+            onMouseOver={() => this.hoverEvent('block', idx)}
+            onMouseOut={() => this.hoverEvent('none', idx)}
+            style={{backgroundImage: `url(${thumbnail})`, backgroundPosition: 'center', height: '100vh', width: `${columnWidth}vw`}}>
+            <Link className="video-column" style={{display: `${displayVersion}`, color: 'white', border: 'solid 5px white'}}>Watch Me</Link>
+          </div>
       )
     });
   }
