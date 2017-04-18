bf0ad5e2163bcdaca93ea0eacc8eed712d8330a0
Keir Staple <keirstaple@comcast.net>
2017-04-17 18:42:30 -0700

    button changes color on hover

--- src/views/components/VideoColumns.js
+++ src/views/components/VideoColumns.js
@@ -36,15 +36,15 @@ class VideoColumns extends Component {
       return(
           <div
             className="video-column"
-            to={`${item.uri}`}
             key={idx}
             onMouseOver={() => this.hoverEvent('block', idx)}
             onMouseOut={() => this.hoverEvent('none', idx)}
             style={{backgroundImage: `url(${thumbnail})`, backgroundPosition: 'center', height: '100vh', width: `${columnWidth}vw`, position: 'relative'}} >
             <Link
               className="video-column-link"
-              style={{display: `${displayVersion}`, color: 'white', border: 'solid 5px white', fontStyle: 'italic', width: '100px', height: 'auto', margin: '0 auto', position: 'absolute', top: '50%', left: '50%', WebkitTransform: 'translate(-50%, -50%)', MsTransform: 'translate(-50%, -50%)', transform: 'translate(-50%, -50%)', textAlign: 'center'}} >
-              Watch Me
+              to={`${item.uri}`}
+              style={{display: `${displayVersion}`, fontStyle: 'italic', width: '100px', height: 'auto', margin: '0 auto', position: 'absolute', top: '50%', left: '50%', WebkitTransform: 'translate(-50%, -50%)', MsTransform: 'translate(-50%, -50%)', transform: 'translate(-50%, -50%)', textAlign: 'center', textDecoration: 'none'}} >
+              WATCH ME
             </Link>
           </div>
       )
