09a928319be857599ae242e2e30bb2a07cf4b8cc
Keir Staple <keirstaple@comcast.net>
2017-05-12 15:13:32 -0700

    add fonts

--- src/views/components/About.js
+++ src/views/components/About.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+// import AkkLight from '../Akkurat/Akkurat-Light.ttf';
 
 class About extends Component {
   // constructor() {
@@ -11,9 +12,9 @@ class About extends Component {
     return (
       <div style={{ position: 'relative'}}>
         <div style={{ position: 'absolute', margin: '40vh auto', textAlign: 'center', top: '50%', left: '50%', WebkitTransform: 'translate(-50%, -50%)', MsTransform: 'translate(-50%, -50%)', transform: 'translate(-50%, -50%)'}}>
-          <h1>CONTACT US</h1>
+          <h1 className="contact-us">CONTACT US</h1>
           <hr />
-          <a href="mailto:Info@potatoportland.com" style={{ textDecoration: 'none', color: 'black' }}>Email: Info@potatoportland.com</a>
+          <a className="email" href="mailto:Info@potatoportland.com" style={{ textDecoration: 'none', color: 'black' }}>Email: Info@potatoportland.com</a>
         </div>
       </div>
     );
