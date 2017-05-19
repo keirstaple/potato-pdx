5a1a0258affd56c5cd623f83084142f97a2aaa1b
Keir Staple <keirstaple@comcast.net>
2017-05-19 14:00:00 -0700

    give contact us section shadow

--- src/views/components/About.js
+++ src/views/components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
     return (
       <div style={{ position: 'relative'}}>
         <div style={{ display: `${this.props.display}`, position: 'absolute', margin: '40vh auto', textAlign: 'center', top: '50%', left: '50%', WebkitTransform: 'translate(-50%, -50%)', MsTransform: 'translate(-50%, -50%)', transform: 'translate(-50%, -50%)'}}>
-          <h1 className="contact-us">CONTACT US</h1>
+          <h1 className="contact-us" style={{ color: '#E89CBA', textShadow: '-7.5px 7.5px #1A1919'}}>CONTACT US</h1>
           <hr />
           <a className="email" href="mailto:Info@potatoportland.com" style={{ textDecoration: 'none', color: 'black' }}>Email: Info@potatoportland.com</a>
         </div>
